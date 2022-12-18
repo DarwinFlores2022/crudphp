ccb15015833e5122067fe825c51c7d519c29b7da
Darwin Flores <darwin2000f@hotmail.com>
2022-12-18 08:49:33 -0600

    CRUD

--- db_tienda.sql
+++ db_tienda.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-12-2022 a las 12:56:22
--- Versión del servidor: 10.4.21-MariaDB
--- Versión de PHP: 8.0.10
+-- Tiempo de generación: 18-12-2022 a las 15:49:08
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `db_tienda`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tb_cliente`
+--
+
+CREATE TABLE `tb_cliente` (
+  `idcliente` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `direccion` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `tb_cliente`
+--
+
+INSERT INTO `tb_cliente` (`idcliente`, `nombre`, `direccion`) VALUES
+(2, 'JOse Juarez', 'Santiago Nonualco, La paz'),
+(3, 'Erick Fernandez', 'Santiago Nonualco, La paz'),
+(4, 'Yolanda Flores', 'Santiago Nonualco, La paz'),
+(5, 'Merrary Flores', 'Santiago Nonualco, La paz'),
+(6, 'OSCAR alexander', 'CLIENTE FRECUENTE DEL NEGOCIO');
 
 -- --------------------------------------------------------
 
@@ -49,11 +72,18 @@ INSERT INTO `tb_producto` (`id`, `nombre`, `descripcion`, `categoria`) VALUES
 (13, 'ASDASD', 'ASDASD', 'Laptops'),
 (14, 'ASDASDASDASD', 'ASDASD', 'Laptops'),
 (17, 'dsfdsfsdfsf', 'darw', 'Laptops'),
-(20, 'CERVEZA', 'cvb', 'Laptops');
+(20, 'CERVEZA', 'cvb', 'Laptops'),
+(23, 'producto 1', '&lt;script&gt;alert(&#039;Hello&#039;);&lt;/script&gt;', 'Tecnolog&iacute;a');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `tb_cliente`
+--
+ALTER TABLE `tb_cliente`
+  ADD PRIMARY KEY (`idcliente`);
 
 --
 -- Indices de la tabla `tb_producto`
@@ -66,10 +96,16 @@ ALTER TABLE `tb_producto`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `tb_cliente`
+--
+ALTER TABLE `tb_cliente`
+  MODIFY `idcliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
 -- AUTO_INCREMENT de la tabla `tb_producto`
 --
 ALTER TABLE `tb_producto`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
